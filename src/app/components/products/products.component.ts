8188557d599c1a28090c993eac4b94aca67d3521
joyce <joycefrontend@gmail.com>
2024-05-19 13:06:07 -0300

    feat: add products

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../../services/products.service';
+import { ProductModel } from '../../models/product.model';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
-export class ProductsComponent {
 
+
+export class ProductsComponent implements OnInit {
+
+  products: ProductModel[] = [];
+
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.productsService.getProducts().subscribe((data: ProductModel[]) => {
+      this.products = data;
+    });
+  }
 }
