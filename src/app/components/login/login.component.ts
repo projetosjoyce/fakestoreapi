61ca2231ce8184855194927bb28ffc5bbeb68d64
joyce <joycefrontend@gmail.com>
2024-05-24 12:43:48 -0300

    feat: removendo style inline

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,39 +2,46 @@ import { Component } from '@angular/core';
 import { LoginService } from '../../services/login.service';
 import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
-import { ModalLoginRegisterComponent } from '../modal-login-register/modal-login-register.component';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-
   username: string = '';
   password: string = '';
   displaySignUpModal: boolean = false;
   visible: boolean = false;
   user: any = {};
 
-  constructor(private authService: LoginService,
-              private router: Router,
-              private messageService: MessageService) {}
+  constructor(
+    private authService: LoginService,
+    private router: Router,
+    private messageService: MessageService
+  ) {}
 
   login(): void {
-    this.authService.login(this.username, this.password)
-      .subscribe(
-        response => {
-          // Se a resposta for bem-sucedida, redirecione para products
-          this.router.navigate(['/products']);
-          this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Logado com sucesso' });
-        },
-        error => {
-          // Se o login falhar, exiba uma mensagem de erro para o usuário
-          console.error('Erro ao fazer login:', error);
-          this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Usuário ou senha incorretos' });
-        }
-      );
+    this.authService.login(this.username, this.password).subscribe(
+      (response) => {
+        // Se a resposta for bem-sucedida, redirecione para products
+        this.router.navigate(['/products']);
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Success',
+          detail: 'Logado com sucesso',
+        });
+      },
+      (error) => {
+        // Se o login falhar, exiba uma mensagem de erro para o usuário
+        console.error('Erro ao fazer login:', error);
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'Usuário ou senha incorretos',
+        });
+      }
+    );
   }
 
   openSignUpModal(event: Event) {
@@ -48,37 +55,36 @@ export class LoginComponent {
 
   showDialog() {
     this.visible = true;
-}
+  }
 
-saveUser() {
-  const userData = {
-    email: this.user.email,
-    username: this.user.username,
-    password: this.user.password,
-    name: {
-      firstname: 'John',
-      lastname:'Doe'
-    },
-    address: {
-      city:'kilcoole',
-      street:'7835 new road',
-      number:3,
-      zipcode:'12926-3874',
-      geolocation: {
-        lat:'-37.3159',
-        long:'81.1496'
-      }
-    },
-    phone:'1-570-236-7033'
-  };
+  saveUser() {
+    const userData = {
+      email: this.user.email,
+      username: this.user.username,
+      password: this.user.password,
+      name: {
+        firstname: 'John',
+        lastname: 'Doe',
+      },
+      address: {
+        city: 'kilcoole',
+        street: '7835 new road',
+        number: 3,
+        zipcode: '12926-3874',
+        geolocation: {
+          lat: '-37.3159',
+          long: '81.1496',
+        },
+      },
+      phone: '1-570-236-7033',
+    };
 
-  fetch('https://fakestoreapi.com/users', {
-    method: 'POST',
-    body: JSON.stringify(userData)
-  })
-  .then(res => res.json())
-  .then(json => console.log(json))
-  .catch(error => console.error('Erro ao criar usuário:', error));
-}
-
+    fetch('https://fakestoreapi.com/users', {
+      method: 'POST',
+      body: JSON.stringify(userData),
+    })
+      .then((res) => res.json())
+      .then((json) => console.log(json))
+      .catch((error) => console.error('Erro ao criar usuário:', error));
+  }
 }
--- src/app/components/modal-login-register/modal-login-register.component.ts
+++ src/app/components/modal-login-register/modal-login-register.component.ts
@@ -7,7 +7,10 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class ModalLoginRegisterComponent {
 
-  visible: boolean = true;
+  visible: boolean = true;  username: string = '';
+  password: string = '';
+  displaySignUpModal: boolean = false;
+  user: any = {};
 
   showDialog() {
       this.visible = true;
@@ -19,6 +22,35 @@ export class ModalLoginRegisterComponent {
     this.close.emit();
   }
 
+  saveUser() {
+    const userData = {
+      email: this.user.email,
+      username: this.user.username,
+      password: this.user.password,
+      name: {
+        firstname: 'John',
+        lastname:'Doe'
+      },
+      address: {
+        city:'kilcoole',
+        street:'7835 new road',
+        number:3,
+        zipcode:'12926-3874',
+        geolocation: {
+          lat:'-37.3159',
+          long:'81.1496'
+        }
+      },
+      phone:'1-570-236-7033'
+    };
 
+    fetch('https://fakestoreapi.com/users', {
+      method: 'POST',
+      body: JSON.stringify(userData)
+    })
+    .then(res => res.json())
+    .then(json => console.log(json))
+    .catch(error => console.error('Erro ao criar usuário:', error));
+  }
 
 }
